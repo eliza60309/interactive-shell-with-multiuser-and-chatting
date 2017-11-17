54a19ede3d3e586831054e06aafb20098c09a638
eliza60309 <eliza60309@gmail.com>
2017-11-17 19:51:09 +0800

    lots of work done

---
+++ multi.cc
@@ -0,0 +1,298 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+//#include <streambuf>
+#include "multi_shell.cc"
+
+#define SERV_TCP_PORT 9487
+
+#define PIPE 30
+#define NAME 1024
+#define MSGSIGN 30
+#define PROCID 20
+#define FDID 10
+
+#define PIPE_SIZE 30 * PIPE
+#define NAME_SIZE 30 * NAME
+#define MSG_SIZE 2048
+#define MSGSIGN_SIZE 1 * MSGSIGN
+#define PROCID_SIZE 30 * PROCID
+#define FDID_SIZE 30 * FDID
+#define SHM_SIZE PIPE_SIZE + NAME_SIZE + MSG_SIZE + MSGSIGN_SIZE + PROCID_SIZE + FDID_SIZE
+#define PIPE_OFFSET 0
+#define NAME_OFFSET PIPE_OFFSET + PIPE_SIZE
+#define MSG_OFFSET NAME_OFFSET + NAME_SIZE
+#define MSGSIGN_OFFSET MSG_OFFSET + MSG_SIZE
+#define PROCID_OFFSET MSGSIGN_OFFSET + MSGSIGN_SIZE
+#define FDID_OFFSET PROCID_OFFSET + PROCID_SIZE
+
+using namespace std;
+
+
+
+class indiv;
+
+int concurrent_connection_oriented(int sock, struct sockaddr *cli_addr);
+int readint(char *c);
+void waitfor(int sig);
+void rcvmsg(int sig);
+int broadcast(const char *c);
+int wisper(int id, char *c);
+void waitterm(int sig);
+int rstmmry(char *c, int size);
+int writeint(char *c, int i);
+void abortion(int sig);
+int writemsg(char *c, const char *str);
+int writemmry(char *c, const char *str);
+string readmmry(char *c);
+
+char *ptr;
+int mysock;
+int myid;
+int shmid;
+
+int main()
+{
+	signal(SIGCHLD, waitfor);
+	signal(SIGTERM, abortion);
+	signal(SIGINT, abortion);
+	int serv_tcp_port = SERV_TCP_PORT;
+	struct sockaddr_in cli_addr, serv_addr;
+	int sock = socket(PF_INET, SOCK_STREAM, 0);
+	mysock = sock;
+	if(sock < 0)
+	{
+		cout << "[ERR] Cannot open socket" << endl;
+		return 0;
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	while(1)
+	{
+		serv_addr.sin_port = htons(serv_tcp_port);
+		int bin = bind(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+		if(bin >= 0)break;
+		serv_tcp_port++;
+	}
+	cout << "[LOG] Bind port: " << serv_tcp_port << endl;
+	listen(sock, 5);
+
+	concurrent_connection_oriented(sock, (struct sockaddr *) &cli_addr);
+}
+
+int concurrent_connection_oriented(int sock, struct sockaddr *cli_addr)
+{
+	shmid = shmget(0, SHM_SIZE, IPC_CREAT | 0666);
+	cout << "[LOG] SHMID = " << shmid << endl;
+	ptr = (char *)shmat(shmid, NULL, 0);
+	memset(ptr, 0, SHM_SIZE);
+	while(1)
+	{
+		unsigned int clilen = sizeof(cli_addr);
+		int newsock = accept(sock, cli_addr, &clilen);
+		int child;
+		if((child = fork()) < 0)
+		{
+			cout << "[ERR] Accept error" << endl;
+			return 0;
+		}
+		else if(child == 0) 
+		{
+			signal(SIGUSR1, rcvmsg);
+			signal(SIGCHLD, waitterm);
+			signal(SIGINT, SIG_DFL);
+			signal(SIGTERM, SIG_DFL);
+			close(sock);
+			for(int i = 0; i < 30; i++)
+			{
+				if((ptr + PROCID_OFFSET + i * PROCID)[0] == 0)
+				{
+					cout << "[LOG] User " << i + 1 << " (" << getpid() << ") connected." << endl;
+					myid = i;
+					mysock = newsock;
+					writeint(ptr + PROCID_OFFSET + i * PROCID, getpid());
+					greeting(newsock);
+					broadcast("*** User '(no name)' entered from CGILAB/511. ***\n");
+					writemmry(ptr + NAME_OFFSET + i * NAME, "(no name)");
+					break;
+				}
+			}
+			indiv user;
+			write(newsock, "% ", 2);
+			while(1)
+			{
+				int ret = process_handler(newsock, user, ptr, myid);
+				if(ret == 0)
+				{
+					string str = string() + "*** User '" + (ptr + NAME_OFFSET + NAME * myid) + "' left. ***\n";
+					broadcast(str.c_str());
+					close(newsock);
+					exit(0);
+				}
+				write(newsock, "% ", 2);
+			}
+		}
+		close(newsock);
+	}
+}
+
+void abortion(int sig)
+{
+	shmdt(ptr);
+	int k = shmctl(shmid, IPC_RMID, NULL);
+	if(k == -1)cout << "[WRN] SHM delete failed." << endl; 
+	else cout << "[LOG] SHM deleted." << endl;
+	exit(0);
+}
+
+int readint(char *c)
+{
+	int i = -1;
+	while(1)
+	{
+		if(c[0] > '9' || c[0] < '0')return i;
+		if(i < 0)i = 0;
+		i *= 10;
+		i += c[0] - '0';
+		c++;
+	}
+}
+
+int writeint(char *c, int i)
+{
+	char b[20] = {};
+	sprintf(b, "%d", i);
+	for(int i = 0; i < 20; i++)
+	{
+		if(b[i] == 0)break;
+		else(c[i] = b[i]);
+	}
+	return 0;
+}
+
+int rstmmry(char *c, int size)
+{
+	for(int i = 0; i < size; i++)c[i] = 0;
+}
+
+int writemsg(char *c, const char *str)
+{
+	int i = 0;
+	for(int k = 0; k < MSG_SIZE; k++)c[k] = 0;
+	while(str[0] != 0)
+	{
+		c[0] = str[0];
+		c++;
+		str++;
+		i++;
+		if(i >= MSG_SIZE - 10)
+		{
+			cout << "[WRN] MSG size is dangerous" << endl;
+			break;
+		}
+	}
+	return 0;
+}
+
+string readmmry(char *c)
+{
+	char buf[10000] = {};
+	int i = 0;
+	while(c[i] != 0)
+	{
+		buf[i] = c[i];
+		i++;
+	}
+	return string(buf);
+}
+
+int writemmry(char *c, const char *str)
+{
+	for(int i = 0;; i++)
+	{
+		if(str[i] == 0)return 0;
+		c[i] = str[i];
+	}
+}
+
+void waitterm(int sig)
+{
+	int wstat;
+	int pid = wait(&wstat);
+}
+
+void waitfor(int sig)
+{
+	int wstat;
+	int pid = wait(&wstat);
+	for(int i = 0; i < 30; i++)
+	{
+		int gpid = readint(ptr + PROCID_OFFSET + i * PROCID);
+		if(gpid == -1);
+		else if(gpid == pid)
+		{
+			cout << "[LOG] User " << i + 1 << " (" << pid << ") disconnected." << endl;
+			rstmmry(ptr + FDID_OFFSET + i * FDID, FDID);
+			rstmmry(ptr + PROCID_OFFSET + i * PROCID, PROCID);
+			rstmmry(ptr + NAME_OFFSET + i * NAME, NAME);
+			rstmmry(ptr + PIPE_OFFSET + i * PIPE, PIPE);
+			return;
+		}
+	}
+	cout << "[WRN] Clean " << " (" << pid << ") failed." << endl;
+}
+
+void rcvmsg(int sig)
+{
+	char c[MSG_SIZE] = {};
+	for(int i = 0; i < MSG_SIZE - 1; i++)
+	{
+		if((ptr + MSG_OFFSET)[i] == 0)break;
+		else c[i] = (ptr + MSG_OFFSET)[i];
+	}
+	string str = c;
+	write(mysock, str.c_str(), str.size());
+}
+
+int broadcast(const char *c)
+{
+	/*for(int i = 0; i < MSG_SIZE - 1; i++)
+	{
+		if(c[i] == 0)break;
+		else (ptr + MSG_OFFSET)[i] = c[i];
+	}*/
+	writemsg(ptr + MSG_OFFSET, c);
+	for(int i = 0; i < 30; i++)
+	{
+		int pid = readint(ptr + PROCID_OFFSET + i * PROCID);
+		if(pid == -1)continue;
+		kill(pid, SIGUSR1);
+	}
+	return 1;
+}
+
+int wisper(int id, char *c)
+{
+	/*for(int i = 0; i < MSG_SIZE - 1; i++)
+	{
+		if(c[i] == 0)break;
+		else (ptr + MSG_OFFSET)[i] = c[i];
+	}*/
+	writemsg(ptr + MSG_OFFSET, c);
+	int pid = readint(ptr + PROCID_OFFSET + id * PROCID);
+	if(pid == -1)
+	{
+		cout << "[WRN]" << "Wisper to unknown userid " << id << endl;
+		return 0;
+	}
+	kill(pid, SIGUSR1);
+	return 1;
+}
---
+++ single.cc
@@ -0,0 +1,223 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/wait.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <strings.h>
+#include <string.h>
+#include <fstream>
+#include <iostream>
+//#include <streambuf>
+#include "single_shell.cc"
+
+#define SERV_TCP_PORT 9487
+
+#define PIPE 30 * 30 * 1024
+#define NAME 30 * 1024
+#define MESSAGE 30 * 1024 * 10
+#define SHM_SIZE PIPE + NAME + MESSAGE
+#define PIPE_OFFSET 0
+#define NAME_OFFSET PIPE
+#define MESSAGE_OFFSET PIPE + NAME
+// pipe | name | message
+using namespace std;
+
+void waitfor(int sig)
+{
+	int wstat;
+	int pid = wait(&wstat);
+}
+
+class indiv;
+
+int concurrent_connection_oriented(int sock, struct sockaddr *cli_addr);
+int single_process_concurrent(int sock, struct sockaddr *cli_addr);
+
+int main()
+{
+	signal(SIGCHLD, waitfor);
+//	signal(SIGINT, abortion);
+	int serv_tcp_port = SERV_TCP_PORT;
+	struct sockaddr_in cli_addr, serv_addr;
+	int sock = socket(PF_INET, SOCK_STREAM, 0);
+	if(sock < 0)
+	{
+		cout << "Error: Cannot open socket" << endl;
+		return 0;
+	}
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+	while(1)
+	{
+		serv_addr.sin_port = htons(serv_tcp_port);
+		int bin = bind(sock, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+		if(bin >= 0)break;
+//		cout << "Error: Can't bind " << serv_tcp_port << endl;
+		serv_tcp_port++;
+	}
+	cout << "Bind port: " << serv_tcp_port << endl;
+	listen(sock, 5);
+
+	single_process_concurrent(sock, (struct sockaddr *) &cli_addr);
+	//concurrent_connection_oriented(sock, (struct sockaddr *) &cli_addr);
+}
+/*
+int concurrent_connection_oriented(int sock, struct sockaddr *cli_addr)
+{
+	int shmid = shmget(0, SHM_SIZE, IPC_CREAT | 0666);
+	char *ptr = (char *)shmat(shmid, NULL, 0);
+	memset(ptr, 0, SHM_SIZE);
+	while(1)
+	{
+		unsigned int clilen = sizeof(cli_addr);
+		int newsock = accept(sock, cli_addr, &clilen);
+		int child;
+		if((child = fork()) < 0)
+		{
+			cout << "Error: Accept error" << endl;
+			return 0;
+		}
+		else if(child == 0) 
+		{
+			close(sock);
+			indiv user;
+			//void *ptr = NULL;
+			greeting(newsock);
+			write(newsock, "% ", 2);
+			while(process_handler(newsock, user, ptr, vect))write(newsock, "% ", 2);
+			exit(0);
+		}
+		close(newsock);
+	}
+}
+*/
+
+
+int single_process_concurrent(int sock, struct sockaddr *cli_addr)
+{
+	//int shmid = shmget(0, SHM_SIZE, IPC_CREAT | 0666);
+	//char *ptr = (char *)shmat(shmid, NULL, 0);
+	char ptr[SHM_SIZE] = {};
+	memset(ptr, 0, SHM_SIZE);
+	fd_set rfds;
+	fd_set afds;
+	int nfds = getdtablesize();
+	FD_ZERO(&afds);
+	FD_SET(sock, &afds);
+	vector<indiv> vect(30, indiv());
+	vector<string> hstr(30, string());
+	vector<string> pstr(30, string());
+	vector<int> defined(30, -1);//-1 not def
+	while(1)
+	{
+		memcpy(&rfds, &afds, sizeof(rfds));
+		while(select(nfds, &rfds, (fd_set *) NULL, (fd_set *) NULL, (struct timeval *) NULL) < 0)
+		{
+			if(errno == EINTR);
+			else
+			{
+				cout << "Error: Select error: " << strerror(errno) << endl;
+				return 0;
+			}
+		}
+		if(FD_ISSET(sock, &rfds))
+		{
+			unsigned int clilen = sizeof(cli_addr);
+			int newsock = accept(sock, cli_addr, &clilen);
+			if(newsock < 0)
+			{
+				cout << "Error: Accept error" << endl;
+				return 0;
+			}
+			FD_SET(newsock, &afds);
+			int flag = false;
+			for(int i = 0; i < 30; i++)
+			{
+				if(defined[i] == -1)
+				{
+					flag = true;
+					vect[i] = indiv();
+					defined[i] = newsock;
+					char hbuf[NI_MAXHOST], sbuf[NI_MAXSERV];
+					getnameinfo(cli_addr, clilen, hbuf, sizeof(hbuf), sbuf, sizeof(sbuf), NI_NUMERICHOST | NI_NUMERICSERV);
+//					hstr[i] = hbuf;
+//					pstr[i] = sbuf;
+					hstr[i] = "CGILAB";
+					pstr[i] = "511";
+					greeting(newsock);
+					string s = string() + "*** User '(no name)' entered from " + hstr[i] + "/" + pstr[i] + ". ***\n";
+					cout << "New user: Host(" << hstr[i] << "), Port(" << pstr[i] << ")" << endl;
+					for(int j = 0; j < 30; j++)
+					{
+						if(defined[j] != -1)write(defined[j], s.c_str(), s.size());
+					}
+					string name = "(no name)";
+					for(int j = 0; j < name.size(); j++)
+					{
+						ptr[NAME_OFFSET + 30 * i + j] = name[j];
+					}
+					write(newsock, "% ", 2);
+					break;
+				}
+			}
+			if(!flag)
+			{
+				write(newsock, "connection refused FULL HOUSE", 20);
+				(void) close(newsock);
+				FD_CLR(newsock, &afds);
+			}
+		}
+		for(int i = 0; i < nfds; i++)
+		{
+			if(i == sock)continue;
+			if(!FD_ISSET(i, &rfds))continue;
+			int id = -1;
+			for(int j = 0; j < 30; j++)
+			{
+				if(defined[j] == i)
+				{
+					id = j;
+					break;
+				}
+			}
+			if(id == -1)
+			{
+				cout << "Error: find" << endl;
+				break;
+			}
+//			void *ptr = NULL;
+			int ret = process_handler(i, vect[id], ptr, defined, hstr, pstr);
+			if(!ret)
+			{
+				string s = string() + "*** User '" + (ptr + NAME_OFFSET + 30 * id) + "' left. ***\n";
+				for(int i = 0; i < 30; i++)
+				{
+					if(defined[id] != -1)write(defined[i], s.c_str(), s.size());
+				}
+				defined[id] = -1;
+				vect[id] = indiv();
+				(void) close(i);
+				FD_CLR(i, &afds);
+				memset(ptr + NAME_OFFSET + 30 * id, 0, 30);
+				for(int i = 1; i < 31; i++)
+				{
+					char b[10] = {};
+					sprintf(b, "%d-%d", i, id + 1);
+					string str;
+					str = "/tmp/wush60309-" + string(b);
+					struct stat exist;
+					if(stat(str.c_str(), &exist) == 0)
+					{
+						remove(str.c_str());
+						cout << "removed" << str << endl;
+					}
+				}
+				memset(ptr + PIPE_OFFSET + 1024 * 30 * id, 0, 1024 * 30);
+			}
+			else write(i, "% ", 2);
+		}
+	}
+}
+
